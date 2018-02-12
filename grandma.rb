d70f833568261b10aed1a4cb0e8a0d764d3d5159
Berry  Semexan <bsemexan@gmail.com>
2018-02-12 23:37:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(response)
   if response == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif response.upcase
+  elsif  == response.upcase
     "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
